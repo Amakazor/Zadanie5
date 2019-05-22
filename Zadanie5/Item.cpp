5603d503d669df32fe42fe5240d9e5d8f9a498e9
Amakazor <amakazor@gmail.com>
2019-05-22 19:48:40 +0200

    Item class

---
+++ Zadanie5/Item.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "Item.h"
+
+//constructors
+
+Item::Item(){}
+
+Item::Item(std::string _name, int _value) :name(_name), value(_value) {};
+
+//getters
+
+std::string Item::get_name()
+{
+	return name;
+}
+
+int Item::get_value()
+{
+	return value;
+}
+
+//setters
+
+void Item::set_name(std::string _name)
+{
+	name = _name;
+}
+
+void Item::set_value(int _value)
+{
+	value = _value;
+}
